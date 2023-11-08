26f1466d90e9012a9476bf69120e8ec3ad68c478
shovonkumar <mail.shovon2@gmail.com>
2023-11-08 23:55:42 +0600

    news part complete

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,10 +1,21 @@
+import newsBg from "../../assets/images/news.jpg";
 import "./NewsLetter.css";
 
 const NewsLetter = () => {
+  const bgStyle = {
+    backgroundImage: `url(${newsBg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
   return (
-    <section className="news">
-      <div className="container text-center">
+    <section className="news" style={bgStyle}>
+      <div className="container text-center text-white">
         <h2>Enjoy your journey</h2>
+        <button className="button">Sign Up</button>
+        <p>
+          You can also sign up on our <a href="#">newsletter</a>
+        </p>
       </div>
     </section>
   );
